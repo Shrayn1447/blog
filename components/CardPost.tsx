42e6c7a01fcad0ef69a6df73654e2631dee71ebe
Eliseenko Alex <scanel2004@gmail.com>
2024-02-21 02:39:28 +0700

    fix_server_middleware

--- components/CardPost.tsx
+++ components/CardPost.tsx
@@ -1,11 +1,10 @@
-import { db } from '@/configs/prisma';
-import React from 'react'
-import Image from 'next/image';
-import Link from 'next/link';
-import BlogRouterLink from './BlogRouterLink';
-import { unstable_noStore as noStore } from 'next/cache';
+import { db } from "@/configs/prisma";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { unstable_noStore as noStore } from "next/cache";
 interface Post {
-   post : {
+  post: {
     id: number;
     authorId: number;
     createdAt: Date;
@@ -14,30 +13,34 @@ interface Post {
     title: string;
     img: string;
     published: boolean;
-   },
-   
-
+  };
 }
-export default async function CardPost({ post } : Post) {
-  noStore()
+export default async function CardPost({ post }: Post) {
+  noStore();
   const user = await db.user.findUnique({
-      where: {
-        id:post.authorId
-      }
-    }
-  )
+    where: {
+      id: post.authorId,
+    },
+  });
   return (
-    <div className='h-[200px] container mx-auto p-3  max-w-[800px] my-[20px] border rounded-lg dark:bg-[#171717] bg-[#f5f5f5]'>
-        <div className='flex gap-2 items-center'>
-            <Image height={200} width={200} className=' rounded-full h-auto w-[50px]' src={user?.image!} alt='logo' />
-            <p>{user?.name}</p>
-            <p className=''>{`${post.createdAt.getDate()} ${post.createdAt.getMonth()} ${post.createdAt.getFullYear()}`}</p>
-        </div>
-        <div className=' cursor-pointer  overflow-hidden flex justify-center'>
-            <BlogRouterLink refId={post.id.toString()} >{post.title}</BlogRouterLink>
-        </div>
-        <div>
-        </div>
+    <div className="h-[200px] container mx-auto p-3  max-w-[800px] my-[20px] border rounded-lg dark:bg-[#171717] bg-[#f5f5f5]">
+      <div className="flex gap-2 items-center cursor-pointer">
+        <Link href={``}>
+        
+        <Image
+          height={200}
+          width={200}
+          className="cursor-pointer hover:-translate-y-1 rounded-full h-auto w-[50px]"
+          src={user?.image!}
+          alt="logo"
+        />
+        <p>{user?.name}</p></Link>
+        <p className="">{`${post.createdAt.getDate()} ${post.createdAt.getMonth()} ${post.createdAt.getFullYear()}`}</p>
+      </div>
+      <div className=" cursor-pointer text-[40px] font-bold overflow-hidden flex justify-center">
+        <Link href={`/p/${post.id.toString()}`}>{post.title}</Link>
+      </div>
+      <div></div>
     </div>
-  )
+  );
 }
